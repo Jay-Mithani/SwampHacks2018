4f2ea99aa2d2dd57269daeeb2d62c2e9c2c2cb5c
Jay Mithani <jayvmithani@gmail.com>
2018-01-20 20:51:52 -0500

    hjbk

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
-document.getElementById('image-input').onkeydown = function(e){
-   if(e.keyCode == 13){
-     
-   }
-};
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+	apiKey: 'af039f0bfa31445b94f72a332313b346'
+});
